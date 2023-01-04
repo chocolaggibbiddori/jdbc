03ccda5f82e8fdb594c80acfdac21edd9fc51bac
chocolaggibbiddori <didrkd684@naver.com>
2023-01-04 23:34:22 +0900

    test : SQLExceptionTranslator

--- src/test/java/hello/jdbc/service/MemberServiceV4Test.java
+++ src/test/java/hello/jdbc/service/MemberServiceV4Test.java
@@ -2,7 +2,7 @@ package hello.jdbc.service;
 
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepository;
-import hello.jdbc.repository.MemberRepositoryV4_1;
+import hello.jdbc.repository.MemberRepositoryV4_2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
@@ -22,6 +22,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
  * 예외 누수 문제 해결
  * SQLException 제거
  * MemberRepository 인터페이스 의존
+ * SQLExceptionTranslator 적용
  */
 @Slf4j
 @SpringBootTest
@@ -47,7 +48,7 @@ class MemberServiceV4Test {
 
         @Bean
         MemberRepository memberRepository() {
-            return new MemberRepositoryV4_1(dataSource);
+            return new MemberRepositoryV4_2(dataSource);
         }
 
         @Bean
